ec59eabdca3b7e2082ec57c80264de396f6d4e83
Sergio Pichardo <sp88work@gmail.com>
2021-03-04 09:30:41 -0600

    add node.js cloudformation project

---
+++ 03_nodejs_ec2_user_data/scripts/create.sh
@@ -0,0 +1,12 @@
+aws cloudformation create-stack \
+  --stack-name $1 \
+  --template-body file://$2 \
+  --parameters file://$3 \
+  --region=us-east-1
+
+# when file is in sam directory
+# aws cloudformation create-stack \
+#   --stack-name $1 \
+#   --template-body file://$2 \
+#   --parameters file://$3 \
+#   --region=us-east-1
